efaa2cde594e04a9568f7977eb252d3151dddf8b
Laura <805467@unizar.es>
2023-04-12 23:17:14 +0200

    arreglado conjunto siguiente y terminada tabla analisis

--- conjuntos.py
+++ conjuntos.py
@@ -9,22 +9,17 @@ def conjunto_primero_token(token, tokens_recursivos, tokens_terminales, tokens_n
         primero.add(None)
 
     for produccion in producciones[token]:
-        conjuntos_primero = []
-        conjunto_interseccion = tokens_terminales | tokens_no_terminales | {None}
         if produccion is not None:
+            epsilon = True
             for token_prod in produccion:
                 if token_prod not in tokens_recursivos:
                     primero_token = conjunto_primero_token(token_prod, tokens_recursivos | set(token_prod), tokens_terminales, tokens_no_terminales, producciones)
-                    conjuntos_primero.append(primero_token)
-                    conjunto_interseccion &= primero_token
-
-            if None in conjunto_interseccion:
-                primero.add(None)
-
-            for conjunto in conjuntos_primero:
-                primero |= conjunto.difference({None})
-                if None not in conjunto:
-                    break
+                    primero |= primero_token.difference({None})
+                    if None not in primero_token:
+                        epsilon = False
+                        break
+            if epsilon:
+                primero |= {None}
 
     return primero
 
@@ -33,71 +28,89 @@ def conjunto_primero(tokens_terminales, tokens_no_terminales, producciones):
     conjunto_primero = dict()
     for token in tokens_terminales | tokens_no_terminales:
         conjunto_primero[token] = conjunto_primero_token(token, set(token), tokens_terminales, tokens_no_terminales, producciones)
-        #print("PRI(", token, "): ", conjunto_primero_token(token, set(token), tokens_terminales, tokens_no_terminales, producciones))
+        print("PRI(", token, "): ", conjunto_primero_token(token, set(token), tokens_terminales, tokens_no_terminales, producciones))
     return conjunto_primero
 
 def conjunto_siguiente(token_inicial, tokens_terminales, tokens_no_terminales, producciones):
+    conjunto_primero(tokens_terminales, tokens_no_terminales, producciones)
+    siguiente_viejo = dict()
     siguiente = dict()
-    siguiente[token_inicial] = {""}
+    siguiente[token_inicial] = {"$"}
+    while siguiente != siguiente_viejo:
+        siguiente_viejo = siguiente.copy()
+        for token in tokens_no_terminales:
+            for produccion in producciones[token]:
+                if produccion is not None:
+                    for index, token_prod in enumerate(produccion):
+                        conjunto_primero_beta = set()
 
-    for token in tokens_no_terminales:
-        for produccion in producciones[token]:
-            if produccion is not None:
-                for index, token_prod in enumerate(produccion):
-                    conjunto_primero_beta = set()
-                    conjunto_primero_beta_interseccion =  tokens_terminales | tokens_no_terminales | {None}
+                        if token_prod in tokens_no_terminales: # A -> a B b
+                            epsilon = True
+                            # Calculate Pri(þ)
+                            for t in produccion[index+1:]:
+                                conjunto_primero_t = conjunto_primero_token(t, set(t), tokens_terminales, tokens_no_terminales, producciones)
+                                conjunto_primero_beta |= conjunto_primero_t.difference({None})
+                                if None not in conjunto_primero_t:
+                                    epsilon = False
+                                    break
 
-                    if token_prod in tokens_no_terminales: # A -> a B b
-                        for t in produccion[index+1:]:
-                            conjunto_primero = conjunto_primero_token(t, tokens_terminales, tokens_no_terminales, producciones)
-                            conjunto_primero_beta |= conjunto_primero
-                            conjunto_primero_beta_interseccion &= conjunto_primero
+                            if epsilon:
+                                conjunto_primero_beta |= {None}
 
-                        if token_prod in siguiente:
-                            siguiente[token_prod] |= conjunto_primero_beta.difference({None})
-                        else:
-                            siguiente[token_prod] = conjunto_primero_beta.difference({None})
-
-                        if index == len(produccion) or None in conjunto_primero_beta_interseccion:  # A -> a B  or A -> a B b con eps in PRI(b)
                             if token_prod in siguiente:
-                                siguiente[token_prod] |= siguiente[token]
+                                siguiente[token_prod] |= conjunto_primero_beta.difference({None})
                             else:
-                                siguiente[token_prod] = siguiente[token]
+                                siguiente[token_prod] = conjunto_primero_beta.difference({None})
+
+                            if index == len(produccion) or None in conjunto_primero_beta:  # A -> a B  or A -> a B b con eps in PRI(b)
+                                if token_prod in siguiente and token in siguiente:
+                                    siguiente[token_prod] |= siguiente[token]
+                                elif token in siguiente:
+                                    siguiente[token_prod] = siguiente[token]
 
     print("conjunto siguiente:", siguiente)
     return siguiente
+
 
 def construccion_tabla(token_inicial, tokens_terminales, tokens_no_terminales, producciones):
     siguiente = conjunto_siguiente(token_inicial, tokens_terminales, tokens_no_terminales, producciones)
     tabla = dict()
 
     for token_no_terminal in tokens_no_terminales:
-        for token_terminal in tokens_terminales|{""}:
+        for token_terminal in tokens_terminales|{"$"}:
             tabla[token_no_terminal, token_terminal] = []
-            print()
 
     for token in tokens_no_terminales:
         for produccion in producciones[token]:
+            primero = set()
             if produccion is not None:
-                primero = set()
+                epsilon = True
                 for t in produccion:
-                    primero |= conjunto_primero_token(t, tokens_terminales, tokens_no_terminales, producciones)
+                    conj_pri = conjunto_primero_token(t, set(t), tokens_terminales, tokens_no_terminales, producciones)
+                    primero |= conj_pri.difference({None})
+                    if None not in conj_pri:
+                        epsilon = False
+                        break
 
-                for elemento in primero&tokens_terminales:
-                    tabla[token, elemento] = produccion # que añado ?
+                if epsilon:
+                    primero |= {None}
+            else:
+                primero = {None}
 
-                if None in primero:
-                    for b in tokens_terminales&siguiente[token]:
-                        tabla[token, b] = produccion  # que añado ?
-                    if "" in siguiente[token]:
-                        tabla[token, ""] = produccion  # que añado ?
-                        print()
+            for elemento in primero & tokens_terminales:
+                tabla[token, elemento].append(producciones[token].index(produccion))
+
+            if None in primero:
+                for b in tokens_terminales & siguiente[token]:
+                    tabla[token, b].append(producciones[token].index(produccion))
+                if "$" in siguiente[token]:
+                    tabla[token, "$"].append(producciones[token].index(produccion))
 
     for token_no_terminal in tokens_no_terminales:
-        for token_terminal in tokens_terminales|{""}:
+        for token_terminal in tokens_terminales|{"$"}:
             if tabla[token_no_terminal, token_terminal] == dict():
-                tabla[token_no_terminal, token_terminal] = "error" # que añado ?
-                #error
+                tabla[token_no_terminal, token_terminal] = "error"  # que añado ?
+                # error
 
     print(tabla)
     return tabla
