61e8743e25573059c69e082c15bcbab6b910b156
Laura <805467@unizar.es>
2023-08-25 20:12:04 +0200

    lalr al vuelo

--- LALR.py
+++ LALR.py
@@ -1,29 +1,46 @@
 import LR
 
-def conj_LR1(start_token, terminal_tokens, non_terminal_tokens, productions):
-    conj = LR.conj_LR1(start_token, terminal_tokens, non_terminal_tokens, productions)
-    new_conj = []
-    for I in conj:
-        aniadir = True
-        for i, c in enumerate(new_conj):
-            if len(c) == len(I):
-                for j in range(len(I)):
-                    if c[j][0] != I[j][0] or c[j][1] != I[j][1]:
-                        break
+def conj_LR1(first_set, start_token, terminal_tokens, non_terminal_tokens, productions):
+    new = [LR.clausura([(start_token, prod, {'$'}) for prod in productions[start_token]], first_set, terminal_tokens,
+                    non_terminal_tokens, productions)]
+    conjunto_I = []
+    diccionario = dict()
+    bool_new = True
+    while bool_new:
+        bool_new = False
+        for I in new:
+            prods = {token for _, prod, _ in I for token in prod if token != '.'}
+            for token in prods:
+                if (I, token) not in conjunto_I:
+                    conjunto_I.append((I, token))
+                    diccionario[len(conjunto_I) - 1, token] = LR.sucesor(I, token, first_set, terminal_tokens,
+                                                                      non_terminal_tokens, productions)
+                    sucesor_token = diccionario[len(conjunto_I) - 1, token]
                 else:
-                    aniadir = False
-                    break
+                    sucesor_token = diccionario[conjunto_I.index((I, token)), token]
 
-        if aniadir:
-            new_conj.append(I)
-        else:
-            new_conj[i] = []
-            for j in range(len(I)):
-                new_conj[i].append((c[j][0], c[j][1], c[j][2] | I[j][2]))
+                if sucesor_token != [] and sucesor_token not in new:
+                    aniadir = True
+                    for i, c in enumerate(new):
+                        if len(c) == len(I):
+                            for j in range(len(I)):
+                                if c[j][0] != sucesor_token[j][0] or c[j][1] != sucesor_token[j][1]:
+                                    break
+                            else:
+                                aniadir = False
+                                break
 
-    for entrada in new_conj:
+                    if aniadir:
+                        new.append(sucesor_token)
+                    else:
+                        new[i] = []
+                        for j in range(len(I)):
+                            new[i].append((c[j][0], c[j][1], c[j][2] | sucesor_token[j][2]))
+                    bool_new = True
+
+    for entrada in new:
         print(entrada)
-    return new_conj
+    return new
 
 def conj_LR1_bis(first_set, start_token, terminal_tokens, non_terminal_tokens, productions):
     conj = LR.conj_LR1(first_set, start_token, terminal_tokens, non_terminal_tokens, productions)
